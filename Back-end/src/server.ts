14dc8163028a3096a952a181dd707f287f3bb4d9
Lance <lance_hemphill1234@yahoo.com>
2024-06-06 15:19:07 -0400

    connected to mongodb on back end and then connected backend to front end

--- Back-end/src/server.ts
+++ Back-end/src/server.ts
@@ -3,13 +3,67 @@ import 'dotenv/config'
 import express from 'express'
 import bodyParser from 'body-parser'
 
+import cors from 'cors';
+
 const app = express();
 app.use(express());
 app.use(bodyParser.json())
+app.use(cors());
 
 import mongoose from 'mongoose';
 import { MongoClient } from 'mongodb';
 
+
+app.get("/hello", async (req, res) => {
+    const client = new MongoClient(process.env.MONGODB_URI!)
+
+
+    try {
+        await client.connect();
+
+        const db = client.db('SocialApp')
+        const data = await db.collection('User1').find({}).toArray();
+
+        console.log(data)
+        res.json(data)
+    }
+    catch (err) {
+        console.error("Error connecting to database", err)
+    }
+    finally {
+        await client.close()
+    }
+});
+
+// app.put('/api/posts/:userId/upvote', (req, res) => {
+//     const { userId } = req.params;
+//     const post = postInfo.find(a => a.userId === userId)
+//     if(post) {
+//         post.upvotes += 1;
+//         res.send(`The article now has ${post.upvotes}`)
+//     }
+//     else {
+//         res.send("That post does not exist")
+//     }
+
+// })
+
+
+// app.post('/api/posts/:userId/comment', (req, res) => {
+
+//     const { userId } = req.params
+//     const { username, text } = req.body;
+
+//     const post = postInfo.find(a => a.userId === userId);
+
+//     if (post) {
+//         post?.comments.push({ username, text });
+//         res.send(post.comments)
+//     }
+//     else {
+//         res.send("That post does not exist")
+//     }
+// })
 
 
 mongoose.connect(process.env.MONGODB_URI!, {
--- Front-end/src/pages/Home.tsx
+++ Front-end/src/pages/Home.tsx
@@ -1,11 +1,46 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
 import Navbar from "../components/Navbar"
 
+
+interface Data {
+  name: string,
+  _id: string
+}
+
 const Home = () => {
+
+  const [data, setData] = useState<Data[]>([])
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      await axios.get("http://localhost:5000/hello")
+      .then(response => {
+        setData(response.data)
+
+      })
+      .catch(error => console.error(error))
+    }
+
+    fetchData()
+
+  }, [])
+
+  useEffect(() => {
+    console.log("backend data:", data)
+  }, [data])
+
   return (
     <>
     <Navbar />
     <main>
-
+        {data.map(post => (
+          <div key={post._id}>
+            <p>{post.name}</p>
+          </div>
+        ))}
     </main>
     </>
   )
